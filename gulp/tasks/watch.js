278ab16740de175b51d32340750282b1c02582ca
mpineda100 <mpineda360@hotmail.com>
2018-01-20 23:42:30 -0800

    Added gulp tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -11,6 +11,22 @@ gulp.task('watch', function() {
     });
     watch('./app/index.html', function() {
         browserSync.reload();
-    })
+    });
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.setMaxListeners('cssInject');
+    });
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.setMaxListeners('scriptsRefresh');
+    });
 
+
+});
+
+gulp.task('cssInject', function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', function() {
+    browserSync.reload();
 })
